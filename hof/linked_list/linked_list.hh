faca11984ffee1db5a6cfec11ccb717f8bdcc79a
TGMarkiewicz <tomasz.g.markiewicz@gmail.com>
2024-03-05 23:15:33 +0100

    [linked_list] Add to_container + print

--- hof/linked_list/linked_list.hh
+++ hof/linked_list/linked_list.hh
@@ -9,7 +9,10 @@
 #ifndef HOF_LINKED_LIST_LINKED_LIST_HH_
 #define HOF_LINKED_LIST_LINKED_LIST_HH_
 
+#include <iostream>
 #include <utility>
+
+// #include <fmt/core.h>
 
 #include "concepts/visitable/visitable.hh"
 
@@ -38,11 +41,18 @@ public:
     void append(Ts&&...);
 
     template<Visitable<T> Visit_f>
-    void traverse(Visit_f const& visit);
+    void traverse(Visit_f const& visit) const;
+
+    template<typename Container_t>
+    Container_t to_container() const;
+
+    void print() const;
 
 
     // static methods
 
+    template<Visitable<T> Visit_f>
+    static void traverse(node const* head, Visit_f const& visit);
     template<Visitable<T> Visit_f>
     static void traverse(node const& head, Visit_f const& visit);
 
@@ -51,6 +61,10 @@ public:
     template<typename... Ts>
     static void append(linked_list& llist, Ts&&... data);
 
+    template<typename Container_t>
+    static Container_t to_container(linked_list const& llist);
+
+    static void print(linked_list const& llist);
 
 private:
     node* head{nullptr};
@@ -74,44 +88,99 @@ private:
 // static methods implementation(s)
 //
 
-
 template<typename T>
 template<Visitable<T> Visit_f>
-void linked_list<T>::traverse(linked_list<T>::node const& head, Visit_f const& visit) {
-    linked_list<T>::node const* current{&head};
-    do {
-
-        if(visit_result_t::do_end_by_visit == visit(current->data)) {
+void linked_list<T>::traverse(
+    linked_list<T>::node const* head,
+    Visit_f const& visit
+) {
+    while(nullptr != head) {
+        if(visit_result_t::do_end_by_visit == visit(head->data)) {
             return;
         }
-        current = current->next;
-
-    } while(nullptr != current);
+        head = head->next;
+    }
+}
+template<typename T>
+template<Visitable<T> Visit_f>
+void linked_list<T>::traverse(
+    linked_list<T>::node const& head,
+    Visit_f const& visit
+) {
+    traverse(&head, visit);
 }
 
 template<typename T>
 void linked_list<T>::append_single(linked_list<T>& llist, T const& data) {
+    // fmt::print("linked_list<T>::append_single(linked_list<T>&, T const&)\n");
     auto* item = new node{data, nullptr};
+    if(nullptr == llist.head && nullptr == llist.tail) {
+        llist.tail = item;
+        llist.head = item;
+        return;
+    }
     llist.tail->next = item;
     llist.tail = item;
 }
 template<typename T>
 void linked_list<T>::append_single(linked_list<T>& llist, T&& data) {
+    // fmt::print("linked_list<T>::append_single(linked_list<T>&, T&&)\n");
     auto* item = new node{std::move(data), nullptr};
+    if(nullptr == llist.head && nullptr == llist.tail) {
+        llist.tail = item;
+        llist.head = item;
+        return;
+    }
     llist.tail->next = item;
     llist.tail = item;
 }
+
+
 template<typename T>
 template<typename... Ts>
 void linked_list<T>::append(linked_list<T>& llist, Ts const&... data) {
+    // fmt::print("linked_list<T>::append(linked_list<T>&, Ts const&...)\n");
     (linked_list<T>::append_single(llist, data), ...);
 }
 template<typename T>
 template<typename... Ts>
 void linked_list<T>::append(linked_list<T>& llist, Ts&&... data) {
+    // fmt::print("linked_list<T>::append(linked_list<T>&, Ts&&...)\n");
     (linked_list<T>::append_single(llist, std::forward<Ts>(data)), ...);
 }
 
+template<typename T>
+template<typename Container_t>
+Container_t linked_list<T>::to_container(linked_list<T> const& llist) {
+    Container_t result{};
+    // result.reserve(128);
+    linked_list<T>::node* current = llist.head;
+
+    while(nullptr != current) {
+        result.push_back(current->data);
+        current = current->next;
+    }
+
+    return result;
+}
+
+
+template<typename T>
+void linked_list<T>::print(linked_list<T> const& llist) {
+    auto const l_print = [](T const& elem) {
+        std::cout << "elem: " << elem << '\n';
+        return visit_result_t::do_end_normally;
+    };
+    linked_list<T>::traverse(llist.head, l_print);
+    // std::string beg = "[";
+    // auto const l_print = [&](T const& elem) {
+    //     std::cout <<beg <<elem <<"]";
+    //     beg = " -> [";
+    //     return visit_result_t::do_end_normally;
+    // };
+    // linked_list<T>::traverse(llist.head, l_print);
+    // std::cout <<'\n';
+}
 
 template<typename T>
 void linked_list<T>::destroy(node* head) {
@@ -135,7 +204,9 @@ template<typename T>
 linked_list<T>::linked_list() noexcept = default;
 
 template<typename T>
-linked_list<T>::linked_list(node* a_head) noexcept : head(a_head), tail(a_head) {
+linked_list<T>::linked_list(node* a_head) noexcept
+: head(a_head)
+, tail(a_head) {
     while(nullptr != tail->next) {
         tail = tail->next;
     }
@@ -144,16 +215,14 @@ linked_list<T>::linked_list(node* a_head) noexcept : head(a_head), tail(a_head) 
 template<typename T>
 linked_list<T>::linked_list(T const& data)
 : head(new node{data, nullptr})
-, tail(head)
-{
+, tail(head) {
     /* Empty */
 }
 
 template<typename T>
 linked_list<T>::linked_list(T&& data)
 : head(new node{std::move(data), nullptr})
-, tail(head)
-{
+, tail(head) {
     /* Empty */
 }
 
@@ -165,21 +234,34 @@ linked_list<T>::~linked_list() {
 template<typename T>
 template<typename... Ts>
 void linked_list<T>::append(Ts const&... data) {
+    // fmt::print("linked_list<T>::append(Ts const&...)\n");
     linked_list<T>::append(*this, data...);
 }
 template<typename T>
 template<typename... Ts>
 void linked_list<T>::append(Ts&&... data) {
+    // fmt::print("linked_list<T>::append(T&&...)\n");
     linked_list<T>::append(*this, std::forward<Ts>(data)...);
 }
 
 template<typename T>
 template<Visitable<T> Visit_f>
-void linked_list<T>::traverse(Visit_f const& visit) {
-    if(nullptr == head) { return; }
+void linked_list<T>::traverse(Visit_f const& visit) const {
+    if(nullptr == head) {
+        return;
+    }
     linked_list<T>::traverse(*head, visit);
 }
 
+template<typename T>
+template<typename Container_t>
+Container_t linked_list<T>::to_container() const {
+    return linked_list<T>::to_container<Container_t>(*this);
+}
+template<typename T>
+void linked_list<T>::print() const {
+    linked_list<T>::print(*this);
+}
 
 } /* namespace hof */
 
--- hof/linked_list/linked_list.test.cc
+++ hof/linked_list/linked_list.test.cc
@@ -16,6 +16,9 @@
 
 
 #include <memory>
+#include <numeric>
+#include <type_traits>
+
 
 #include <fmt/core.h>
 
@@ -27,21 +30,15 @@
 
 class linked_list_Test : public testing::Test {
 public:
-    virtual void SetUp() override {
-    }
-    virtual void TearDown() override {
-    }
+    virtual void SetUp() override { }
+    virtual void TearDown() override { }
 };
 
-TEST_F(linked_list_Test, fixture_self_test) {
-}
-TEST_F(linked_list_Test, DISABLED_test) {
-}
+TEST_F(linked_list_Test, fixture_self_test) { }
+TEST_F(linked_list_Test, DISABLED_test) { }
 
 TEST_F(linked_list_Test, 2nd_dtor) {
-    {
-        std::make_shared<hof::linked_list<int>>();
-    }
+    { std::make_shared<hof::linked_list<int>>(); }
     {
         hof::linked_list<int> const* ptr = new hof::linked_list<int>();
         delete ptr;
@@ -70,27 +67,100 @@ TEST_F(linked_list_Test, basic_traversal___) {
 }
 
 namespace {
+
 template<typename T>
 struct container_t {
-    container_t(T const& a_data) : data(a_data) {}
-    container_t(T&& a_data) : data(std::move(a_data)) {}
+private:
+    static constexpr bool do_log() { return true; }
+    static constexpr bool do_log_extra() { return false; }
+
+public:
+    container_t() noexcept(noexcept(T()))
+    : data() { }
+    container_t(T const& a_data) noexcept(noexcept(T()))
+    : data(a_data) {
+        if constexpr(do_log() && do_log_extra()) {
+            fmt::print(
+                " > container_t::container_t(T const&).data = [{}]\n", data
+            );
+        }
+    }
+    container_t(T&& a_data) noexcept(std::is_nothrow_move_constructible_v<T>)
+    : data(std::move(a_data)) {
+        if constexpr(do_log() && do_log_extra()) {
+            fmt::print(" > container_t::container_t(T&&).data = [{}]\n", data);
+        }
+    }
     ~container_t() {
-        fmt::print(" > container_t::~container_t().data = [{}]\n", data);
+        if constexpr(do_log()) {
+            fmt::print(" > container_t::~container_t().data = [{}]\n", data);
+        }
     }
 
-    container_t(container_t const& other) : data(other.data) { }
-    container_t& operator=(container_t const& other) {
+    container_t(container_t const& other) noexcept(noexcept(T(T())))
+    : data(other.data) {
+        if constexpr(do_log() && do_log_extra()) {
+            fmt::print(
+                " > container_t::container_t(container_t const&).data <- data "
+                "= [{}] <- [{}]\n",
+                T(),
+                other.data
+            );
+        }
+    }
+    container_t& operator=(container_t const& other
+    ) noexcept(std::is_nothrow_copy_constructible_v<T>) {
         if(&other != this) {
+            if constexpr(do_log() && do_log_extra()) {
+                fmt::print(
+                    " > container_t::operator=(container_t const&).data <- "
+                    "data = [{}] <- [{}]\n",
+                    data,
+                    other.data
+                );
+            }
             data = other.data;
         }
         return *this;
     }
-    container_t(container_t&& other) : data(other.data) { other.data = T(); }
-    container_t& operator=(container_t&& other) {
+    container_t(container_t&& other) noexcept(noexcept(T(T())))
+    : data(std::move(other.data)) {
+        if constexpr(do_log() && do_log_extra()) {
+            fmt::print(
+                " > container_t::container_t(container_t&&).data <- data = "
+                "[{}] <- [{}]\n",
+                T(),
+                other.data
+            );
+        }
+        other.data = T();
+    }
+    container_t& operator=(container_t&& other
+    ) noexcept(std::is_nothrow_move_constructible_v<T>) {
+        if constexpr(do_log() && do_log_extra()) {
+            fmt::print(
+                " > container_t::operator=(container_t&&).data <- data = [{}] "
+                "<- [{}]\n",
+                data,
+                other.data
+            );
+        }
         data = other.data;
         other.data = T();
 
         return *this;
+    }
+
+    friend bool operator==(container_t const& l, container_t const& r) {
+        return l.data == r.data;
+    }
+    friend bool operator!=(container_t const& l, container_t const& r) {
+        return !(l == r);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, container_t const& c) {
+        os << c.data;
+        return os;
     }
 
 
@@ -116,24 +186,62 @@ TEST_F(linked_list_Test, create_and_add_elements) {
         {
             auto const d = elem_t{'d'};
             ll.append(d);
+            ASSERT_EQ('d', d.data);
         }
         hof::linked_list<elem_t>::append(ll, 'e', 'f');
         {
             auto const g = elem_t{'g'};
             auto const h = elem_t{'h'};
             ll.append(g, h);
+            ASSERT_EQ('g', g.data);
+            ASSERT_EQ('h', h.data);
         }
         {
             auto i = elem_t{'i'};
             auto j = elem_t{'j'};
             ll.append(i, std::move(j), 'k', elem_t{'l'});
+            ASSERT_EQ('i', i.data);
+            ASSERT_EQ('\0', j.data); // We std::move-d j!
         }
 
         ll.traverse(print);
     }
-
 }
 
+TEST_F(linked_list_Test, to_container___) {
+    using elem_t = container_t<std::int32_t>;
+
+
+    // std::vector<elem_t> const expected{1, 2, 3, 4, 5, 6, 7, 13};
+    // // std::vector<elem_t> const expected = []{
+    // //     std::vector<elem_t> result(7, elem_t());
+    // //     std::iota(std::begin(result), std::end(result), 1);
+    // //     return result;
+    // // }();
+
+    std::vector<elem_t> const expected = [] {
+        std::vector<elem_t> result{};
+        result.reserve(64);
+        for(std::int32_t i = 1; 8 > i; ++i) {
+            result.emplace_back(i);
+            // result.push_back(i);
+        }
+
+        return result;
+    }();
+
+
+    hof::linked_list<elem_t> llist{};
+    for(auto const& e : expected) {
+        llist.append(e);
+    }
+
+    llist.print();
+
+    auto const actual = llist.to_container<std::vector<elem_t>>();
+
+    ASSERT_EQ(expected, actual);
+}
 #ifdef __clang__
 #pragma clang diagnostic pop
 #endif /* __clang__ */
